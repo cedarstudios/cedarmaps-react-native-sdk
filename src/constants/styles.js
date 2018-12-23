0fef98315f780a89168177edce9bbb6c0bb76fd5
Sina Abadi <s.abadi@kikojas.com>
2018-12-23 14:42:49 +0330

    Add light raster style.

--- src/constants/styles.js
+++ src/constants/styles.js
@@ -1,2 +1,3 @@
-export const DARK_STYLE_URL = 'http://api-qa.cedarmaps.com/v1/styles/cedarmaps.dark.json'
-export const LIGHT_STYLE_URL = 'http://api-qa.cedarmaps.com/v1/styles/cedarmaps.light.json'
+export const DARK_STYLE_URL = 'https://api.cedarmaps.com/v1/styles/cedarmaps.dark.json'
+export const LIGHT_STYLE_URL = 'https://api.cedarmaps.com/v1/styles/cedarmaps.light.json'
+export const LIGHT_RASTER_STYLE_URL = 'https://api.cedarmaps.com/v1/tiles/light.json'
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Mapbox from '@cedarstudios/react-native-mapbox-gl'
-import { DARK_STYLE_URL, LIGHT_STYLE_URL } from './constants/styles'
+import { DARK_STYLE_URL, LIGHT_STYLE_URL, LIGHT_RASTER_STYLE_URL } from './constants/styles'
 import { getToken } from './helpers/auth'
 import { View } from 'react-native'
 
@@ -9,11 +9,14 @@ Mapbox.setAccessToken('pk.1234')
 const styleMapper = {
   'style://streets-light': LIGHT_STYLE_URL,
   'style://streets-dark': DARK_STYLE_URL,
+  'style://streets-light-raster': LIGHT_RASTER_STYLE_URL,
+
 }
 
 const backgroundColorMapper = {
   LIGHT_STYLE_URL: '#f4f3f0',
   DARK_STYLE_URL: '#213945',
+  LIGHT_RASTER_STYLE_URL: '#f4f3f0',
 }
 
 class CedarMaps extends Component<{}> {
@@ -46,7 +49,7 @@ class CedarMaps extends Component<{}> {
     const cedarMapStyle = styleMapper[mapStyle] || LIGHT_STYLE_URL
     if (!token) {
       return (<View style={{
-        ...this.props.style,
+        flex: 1,
         backgroundColor: backgroundColorMapper[cedarMapStyle],
       }}/>)
     }
