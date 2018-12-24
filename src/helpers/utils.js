f4656ecef5604589a92784d4c4d75778c882fee9
Sina Abadi <s.abadi@kikojas.com>
2018-12-24 17:27:02 +0330

    Fix clean url function when null url provided

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,4 +1,5 @@
 export const cleanUrl = url => {
+  if(!url) return
   let modifiedUrl = url.replace(/\/$/, '')
   if (!/^(?:f|ht)tps?\:\/\//.test(modifiedUrl)) modifiedUrl = `https://${modifiedUrl}`
   return modifiedUrl
